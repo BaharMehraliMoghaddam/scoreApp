710d709f4b1857259b9c807a638a90affa41c87a
Bahar Mehrali Moghaddam <bahar.6304118@gmail.com>
2024-07-25 07:14:11 -0700

    third commit

--- src/components/ScoreRow.jsx
+++ src/components/ScoreRow.jsx
@@ -3,6 +3,9 @@ export default function ScoreRow({scoreDetale}) {
     const color = hue(scoreDetale.A)
     const  backGround=hue(scoreDetale.A , "10%")
   return (
-    <div className={`w-full p-3 rounded-xl`} style={{color:color, backgroundColor:backGround }}>{scoreDetale.category}</div>
+    <div className={`w-full p-3 rounded-xl flex items-center justify-between`} style={{color:color, backgroundColor:backGround }}>
+        <div className="flex items-center justify-center gap-2"><span className=""><i class={`fa-solid ${scoreDetale.icon}`}></i></span><p>{scoreDetale.category}</p></div>
+        <div className="flex items-center justify-center gap-2"><p className="font-semibold text-gray-900">{scoreDetale.score} </p><span className="text-gray-400"> / 100</span></div>
+    </div>
   )
 }
